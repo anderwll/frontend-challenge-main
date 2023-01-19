bccd5d36d4391507f5385335618fb55d664ba3fe
Anderson Wilmsen <ander.wilmsen98@gmail.com>
2023-01-19 17:33:16 -0300

    Adc alert p/ pag de gerencia de conta

--- frontend/src/pages/Management/index.tsx
+++ frontend/src/pages/Management/index.tsx
@@ -1,14 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import LoginModal from '../../components/Modals/LoginModal';
 import { Container } from '../styled';
 
 const ManagementPage = () => {
+  const [openModalLogin, setOpenModalLogin] = useState(false);
+
   useEffect(() => {
-    document.title = 'Gerência de conta | Refresher';
+      document.title = 'Gerência de conta | Refresher';
+      setOpenModalLogin(true);
+      alert('Necessário login...');
     }, []);
 
   return (
     <Container>
       <h1>GERÊNCIA DE CONTA</h1>
+
+      <LoginModal isOpen={openModalLogin} handleClose={() => setOpenModalLogin(false)} />
     </Container>
   )
 };
